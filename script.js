ff378445296887eb88e5b57ff1bcf9304c545126
Zam <samuel.kodehode@gmail.com>
2024-01-11 14:54:36 +0100

    added stuff

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ for (let nftCount = 0; nftCount < 200; nftCount++){
     let nft2 = document.createElement("img")
     let nft3 = document.createElement("img")
     let nft4 = document.createElement("img")
+    let nft5 = document.createElement("img")
+
 
     nftContainer.style.top = `${(nftCount * 50) + 23}%`
     nftContainer.style.left = `47%`
@@ -29,24 +31,28 @@ for (let nftCount = 0; nftCount < 200; nftCount++){
     nft2.style.filter = `drop-shadow(0 100px ${getRandomColor()})`
     nft3.style.filter = `drop-shadow(0 100px ${getRandomColor()})`
     nft4.style.filter = `drop-shadow(0 100px ${getRandomColor()})`
+    nft5.style.filter = `drop-shadow(0 100px ${getRandomColor()})`
 
     nft0.src = `img/0.png`
     nft1.src = `img/1.png`
     nft2.src = `img/2.png`
     nft3.src = `img/3.png`
     nft4.src = `img/hat-hair/hat-${getRandomNumberInRange(1, 11)}.png`
+    nft5.src = `img/acs/acs-${getRandomNumberInRange(1, 15)}.png`
 
     nft0.classList.add("image-parts")
     nft1.classList.add("image-parts")
     nft2.classList.add("image-parts")
     nft3.classList.add("image-parts")
     nft4.classList.add("image-parts")
+    nft5.classList.add("image-parts")
 
     nftContainer.appendChild(nft0)
     nftContainer.appendChild(nft1)
     nftContainer.appendChild(nft2)
     nftContainer.appendChild(nft3)
     nftContainer.appendChild(nft4)
+    nftContainer.appendChild(nft5)
 
 }
 
